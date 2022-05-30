e82858fd53675d67899bb02ea5b29090520e3cdf
BinaryGreen <dannymango11@gmail.com>
2022-05-30 03:10:01 -0400

    working on styling and fetching from REST Countries API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Menu from './Components/Menu';
 import Game from './Components/Game';
 import Results from './Components/Results';
 import './App.css'
+import { ViewStateContext } from './Helpers/Contexts';
 
 function App() {
 
@@ -10,12 +11,14 @@ function App() {
 
   return (
       <div className="flex flex-col min-h-screen justify-center items-center gap-36 bg-gradient-to-br from-green-300 to-green-100">
-        <h1 className="text-6xl font-light tracking-widest">geoquiz.</h1>
-        <div className="m-0 p-0">
-          {viewState === 'menu' && <Menu />}
-          {viewState === 'game' && <Game />}
-          {viewState === 'results' && <Results />}
-        </div>
+        <h1 className="absolute top-12 text-6xl font-light tracking-widest">geoquiz.</h1>
+        <ViewStateContext.Provider value={{ viewState, setViewState }}>
+          <div className="m-0 p-0">
+            {viewState === 'menu' && <Menu />}
+            {viewState === 'game' && <Game />}
+            {viewState === 'results' && <Results />}
+          </div>
+        </ViewStateContext.Provider>
       </div>
   );
 }
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+
+const URL = 'https://restcountries.com/v3.1/all';
+
+const fetchData = async () => {
+  const res = await axios.get(URL);
+  console.log(res);
+  const correctAnswerArray = [];
+
+  for (let i = 0; i < 10; i++) {
+    let country = res.data[Math.floor(Math.random() * res.data.length)];
+    let idx = res.data.indexOf(country);
+    res.data.splice(idx, 1);
+    correctAnswerArray.push(country);
+  }
+  console.log(correctAnswerArray);
+  console.log(res.data.length);
+}
 
 const Game = () => {
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [currentChoices, setCurrentChoices] = useState([]);
+  const [correctAnswer, setCorrectAnswer] = useState('');
+  const [score, setScore] = useState(0);
+
+  /*
+    fetch API and place into array UNEDITED, score init 0 and currentQuestion init 1
+    get 4 random countries and place into another array CORRECT ANSWERS
+    4 buttons (choices) map and filter over array to randomly choose 3 countries not equal to those in CORRECT ANSWERS
+    if button is clicked which contains country object in CORRECT ANSWERS = correct
+      - setTimeOut, show correct and incorrect answers, score++, currentQuestion++ and re-map
+    repeat until (for now) currentQuestion = CORRECT ANSWERS.length (test with 5), setViewState('results')
+  */
+
+  useEffect(() => {
+
+    fetchData();
+  }, [])
+
   return (
-    <div>Game</div>
+    <div className='container'>
+      <div className='bg-teal-200/75 text-black p-10 pb-4 rounded-xl shadow-md'>
+        <p className='text-2xl pb-4'>
+          What is the capital of COUNTRY_CORRECT ANSWER?
+        </p>
+        <div className='flex flex-wrap mt-4 justify-around'>
+          <button className='bg-white w-5/12 p-4 text-black font-semibold rounded shadow mb-4
+          transition duration-300 ease-in-out hover:scale-110'>
+            CHOICE 1
+          </button>
+          <button className='bg-white w-5/12 p-4 text-black font-semibold rounded shadow mb-4
+          transition duration-300 ease-in-out hover:scale-110'>
+            CHOICE 2
+          </button>
+          <button className='bg-white w-5/12 p-4 text-black font-semibold rounded shadow mb-4
+          transition duration-300 ease-in-out hover:scale-110'>
+            CHOICE 3
+          </button>
+          <button className='bg-white w-5/12 p-4 text-black font-semibold rounded shadow mb-4
+          transition duration-300 ease-in-out hover:scale-110'>
+            CHOICE 4
+          </button>
+        </div>
+        <p className='text-center text-xl'>{score} / 10</p>
+      </div>
+    </div>
   )
 }
 
